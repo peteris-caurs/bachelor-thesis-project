d33315cb57bc310ccafa12bc8d7cd0896ab43a40
peteris-caurs <peteris.caurs@gmail.com>
2018-04-24 00:28:13 +0300

    Removed database setup code

--- graphql-server/src/main/java/com/peteriscaurs/graphqlserver/graphqlserver/GraphqlServerApplication.java
+++ graphql-server/src/main/java/com/peteriscaurs/graphqlserver/graphqlserver/GraphqlServerApplication.java
@@ -15,34 +15,10 @@ import java.util.stream.Collectors;
 import static java.util.stream.Collectors.toList;
 
 @SpringBootApplication
-public class GraphqlServerApplication implements CommandLineRunner {
-
-	@Autowired
-	private WordsRepository wordsRepository;
+public class GraphqlServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GraphqlServerApplication.class, args);
-	}
-
-	@Override
-	public void run(String... strings) throws Exception {
-		List<Word> words = wordsRepository.findAll();
-
-		List<Word> updatedWords = words.stream()
-				.map(word ->
-						new Word(
-								word.getId(),
-								word.getHeadword().toUpperCase(),
-								word.getPosTag(),
-								word.getSources(),
-								WordScoreCalculator.calculateWordScore(word.getHeadword()),
-								word.getHeadword().length())
-				)
-				.filter(word -> WordEligabilityChecker.isEligable(word.getHeadword()))
-				.filter(word -> word.getLength() <= 15)
-				.collect(toList());
-
-		updatedWords.forEach(System.out::println);
 	}
 
 }
