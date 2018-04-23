3c0a310e8393a491d82b32425348fc33ff6c22b1
peteris-caurs <peteris.caurs@gmail.com>
2018-04-24 00:35:05 +0300

    Optimized imports

--- graphql-server/src/main/java/com/peteriscaurs/graphqlserver/graphqlserver/GraphqlServerApplication.java
+++ graphql-server/src/main/java/com/peteriscaurs/graphqlserver/graphqlserver/GraphqlServerApplication.java
@@ -1,18 +1,7 @@
 package com.peteriscaurs.graphqlserver.graphqlserver;
 
-import com.peteriscaurs.graphqlserver.graphqlserver.domain.Word;
-import com.peteriscaurs.graphqlserver.graphqlserver.repositories.WordsRepository;
-import com.peteriscaurs.graphqlserver.graphqlserver.utilities.WordEligabilityChecker;
-import com.peteriscaurs.graphqlserver.graphqlserver.utilities.WordScoreCalculator;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static java.util.stream.Collectors.toList;
 
 @SpringBootApplication
 public class GraphqlServerApplication {
