bd01c75eba039cd16af753469aa450f753dd2d2f
peteris-caurs <peteris.caurs@gmail.com>
2018-04-29 20:19:13 +0300

    Created User class

---
+++ graphql-server/src/main/java/com/peteriscaurs/graphqlserver/graphqlserver/domain/User.java
@@ -0,0 +1,66 @@
+package com.peteriscaurs.graphqlserver.graphqlserver.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * @author peteris-caurs
+ */
+@Document(collection = "users")
+public class User {
+
+    @Id private String id;
+    private String name;
+    private String email;
+    private String password;
+
+    public User(String id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+}
