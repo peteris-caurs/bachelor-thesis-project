a1a2a535e0a9701fbff02a85ba69dff9111cc1fb
peteris-caurs <peteris.caurs@gmail.com>
2018-04-29 20:20:32 +0300

    Created Mutation resolver

---
+++ graphql-server/src/main/java/com/peteriscaurs/graphqlserver/graphqlserver/resolvers/Mutation.java
@@ -0,0 +1,27 @@
+package com.peteriscaurs.graphqlserver.graphqlserver.resolvers;
+
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
+import com.peteriscaurs.graphqlserver.graphqlserver.domain.CreateUserInput;
+import com.peteriscaurs.graphqlserver.graphqlserver.domain.User;
+import com.peteriscaurs.graphqlserver.graphqlserver.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * @author peteris-caurs
+ */
+@Component
+public class Mutation implements GraphQLMutationResolver {
+
+    private UserService userService;
+
+    @Autowired
+    public Mutation(UserService userService) {
+        this.userService = userService;
+    }
+
+    public User createUser(CreateUserInput createUserInput) {
+        return userService.createUser(createUserInput);
+    }
+}
