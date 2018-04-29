fceaf571bd299301bdbc6913ea26a774c40048e0
peteris-caurs <peteris.caurs@gmail.com>
2018-04-29 20:15:31 +0300

    ...

--- graphql-server/src/main/java/com/peteriscaurs/graphqlserver/graphqlserver/domain/Word.java
+++ graphql-server/src/main/java/com/peteriscaurs/graphqlserver/graphqlserver/domain/Word.java
@@ -4,8 +4,6 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 /**
- * Scrabble word
- *
  * @author peteris-caurs
  */
 @Document(collection = "words")
