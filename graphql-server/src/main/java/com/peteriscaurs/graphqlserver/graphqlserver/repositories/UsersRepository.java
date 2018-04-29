4dcddf826e5907416ef68587ed1c92e8c2208d0f
peteris-caurs <peteris.caurs@gmail.com>
2018-04-29 20:18:46 +0300

    ...

--- graphql-server/src/main/java/com/peteriscaurs/graphqlserver/graphqlserver/repositories/UsersRepository.java
+++ graphql-server/src/main/java/com/peteriscaurs/graphqlserver/graphqlserver/repositories/UsersRepository.java
@@ -1,10 +1,10 @@
 package com.peteriscaurs.graphqlserver.graphqlserver.repositories;
 
-import com.peteriscaurs.graphqlserver.graphqlserver.domain.Word;
+import com.peteriscaurs.graphqlserver.graphqlserver.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 /**
  * @author peteris-caurs
  */
-public interface WordsRepository extends MongoRepository<Word, String> {
+public interface UsersRepository extends MongoRepository<User, String> {
 }
