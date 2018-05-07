0cab8703450a0a1c9df7ded2576feeab319477ef
peteris-caurs <peteris.caurs@gmail.com>
2018-05-07 23:28:30 +0300

    Created about/scrabble page

---
+++ apollo-client/src/app/scrabble/scrabble.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ScrabbleComponent } from './scrabble.component';
+
+describe('ScrabbleComponent', () => {
+  let component: ScrabbleComponent;
+  let fixture: ComponentFixture<ScrabbleComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ScrabbleComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ScrabbleComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ apollo-client/src/app/scrabble/scrabble.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-scrabble',
+  templateUrl: './scrabble.component.html',
+  styleUrls: ['./scrabble.component.scss']
+})
+export class ScrabbleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
