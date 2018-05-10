9647f56f034961f1129676dea68c5ba32db49442
peteris-caurs <peteris.caurs@gmail.com>
2018-05-10 23:13:32 +0300

    Implemented signUp mutation

--- apollo-client/src/app/register/register.component.ts
+++ apollo-client/src/app/register/register.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { User } from '../graphql/types';
+import { Subscription } from 'rxjs/Subscription';
+import { Apollo } from 'apollo-angular';
+import { SIGNUP_USER_MUTATION, SignUpUserMutationResponse } from '../graphql/queries-mutations';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  name: string;
+  email: string;
+  password: string;
+
+  user: User = new User();
+
+  private subscriptions: Subscription[] = [];
+
+  constructor(private apollo: Apollo) { }
 
   ngOnInit() {
+  }
+
+  signUp() {
+    console.log(this.name);
+    console.log(this.email);
+    console.log(this.password);
+
+    this.user.name = this.name;
+    this.user.email = this.email;
+    this.user.password = this.password;
+
+    const SignUpUserMutationSubscription = this.apollo.mutate<SignUpUserMutationResponse>({
+      mutation: SIGNUP_USER_MUTATION,
+      variables: {
+        name: this.name,
+        email: this.email,
+        password: this.password
+      },
+    })
+      .subscribe((response) => {
+      });
+
+    this.subscriptions = [...this.subscriptions, SignUpUserMutationSubscription];
+  }
+
+  ngOnDestroy() {
+    for (const sub of this.subscriptions) {
+      if (sub && sub.unsubscribe) {
+        sub.unsubscribe();
+      }
+    }
   }
 
 }
